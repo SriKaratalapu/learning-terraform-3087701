802620c59abf9edc1959684a4cc988ea4a26f406
Sri Krishna Karatalapu <srikrishna.karatalapu@gmail.com>
2024-12-02 21:31:52 -0800

    Update main.tf
    fixing missing rule typo

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
   vpc_id       = data.aws_vpc.default.id
 }
 
-resource "aws_security_group" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type      = "ingress"
   from_port = 80
   to_port   = 80
@@ -47,7 +47,7 @@ resource "aws_security_group" "blog_http_in" {
 }
 
 
-resource "aws_security_group" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
@@ -58,7 +58,7 @@ resource "aws_security_group" "blog_https_in" {
 }
 
 
-resource "aws_security_group" "blog_everythin_out" {
+resource "aws_security_group_rule" "blog_everythin_out" {
   type      = "egress"
   from_port = 0
   to_port   = 0
